c3ef7345840ea70e73e3b8baeef15b99b90ad507
marijavelichkovska <marija.velickovska.55@gmail.com>
2020-01-06 21:35:44 +0100

    Employee Controller added

--- PoolSystem/PoolSystem/Controllers/EmployeeController.cs
+++ PoolSystem/PoolSystem/Controllers/EmployeeController.cs
@@ -10,15 +10,46 @@ namespace PoolSystem.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class EmployeeController : Controller
+    public class EmployeesController : Controller
     {
         private readonly PoolSystemContext dbContext;
 
-        public EmployeeController(PoolSystemContext dbContext)
+        public EmployeesController(PoolSystemContext dbContext)
         {
             this.dbContext = dbContext;
         }
         //[HttpPost()]
         //public Employee addNewEmployee(int Id, )
+
+
+        [HttpGet]
+
+        public List<Employee> allEmployees()
+        {
+            return dbContext.Employee.Where(emp => emp.DeletedOn == null).ToList();
+
+        }
+        [HttpDelete]
+        public Employee deleteEmployee(int id)
+        {
+            Employee emp = dbContext.Employee.Find(id);
+            emp.DeletedOn = DateTime.Now;
+            dbContext.SaveChanges();
+            return emp;
+        }
+        [HttpPost]
+        [Route("add")]
+        public Employee addEmployee(Employee emp)
+        {
+            Employee employee = new Employee();
+            employee.Address = emp.Address;
+            employee.FirstName = emp.FirstName;
+            employee.LastName = emp.LastName;
+            employee.Phone = emp.Phone;
+            employee.Id = emp.Id;
+            dbContext.Employee.Add(employee);
+            dbContext.SaveChanges();
+            return employee;
+        }
     }
 }
