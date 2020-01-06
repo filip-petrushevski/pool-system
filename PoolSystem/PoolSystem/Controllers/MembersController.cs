8015c44ec871e3262e41c25dd257c5f6463eb2ca
Angela Kralevska <superangelangela@hotmail.com>
2020-01-06 21:25:58 +0100

    member controller made

---
+++ PoolSystem/PoolSystem/Controllers/MembersController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PoolSystem.DbContexts;
+using PoolSystem.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PoolSystem.Controllerss
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MembersController : Controller
+    {
+        private readonly PoolSystemContext dbContext;
+
+        public MembersController(PoolSystemContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet()]
+        public IEnumerable<Member> getAllMembers()
+        {
+            return dbContext.Member.ToList();
+        }
+
+        [HttpPost()]
+        public Member addMember(Member member)
+        {
+            dbContext.Member.Add(member);
+            dbContext.SaveChanges();
+            return member;
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Member> removeMember(int id)
+        {
+            Member memberToRemove = dbContext.Member.FirstOrDefault(mem => mem.Id == id);
+
+            if (memberToRemove == null)
+            {
+                return NotFound();
+            }
+            dbContext.Member.Remove(memberToRemove);
+            dbContext.SaveChanges();
+            return memberToRemove;
+        }
+    }
+}
--- PoolSystem/PoolSystemDatabase/Tables/Member.sql
+++ PoolSystem/PoolSystemDatabase/Tables/Member.sql
@@ -5,6 +5,6 @@
 	[LastName] NVARCHAR(40) NOT NULL,
 	[Address] NVARCHAR(100) NOT NULL,
 	[Phone] VARCHAR(20) NOT NULL,
-	[LastPaymentId] INT NOT NULL, 
+	[LastPaymentId] INT NULL, 
     CONSTRAINT [FK_Member_Payment] FOREIGN KEY ([LastPaymentId]) REFERENCES [Payment]([Id])
 )
