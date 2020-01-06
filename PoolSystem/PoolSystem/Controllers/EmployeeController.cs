600f9d492482cf673749ad5216a0663ff32210c6
Angela Kralevska <superangelangela@hotmail.com>
2020-01-06 17:12:09 +0100

    recisi prazen kontroler

---
+++ PoolSystem/PoolSystem/Controllers/EmployeeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PoolSystem.DbContexts;
+using PoolSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PoolSystem.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : Controller
+    {
+        private readonly PoolSystemContext dbContext;
+
+        public EmployeeController(PoolSystemContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        //[HttpPost()]
+        //public Employee addNewEmployee(int Id, )
+    }
+}
