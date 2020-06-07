a81ec53963eff5d0f404de457fe03e35b49661b7
Filip Petrushevski <filip.petrus@hotmail.com>
2020-06-08 01:09:51 +0200

    Deleted garbage code

--- PoolSystem/PoolSystem/Controllers/PaymentsController.cs
+++ PoolSystem/PoolSystem/Controllers/PaymentsController.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PoolSystem.DbContexts;
 using PoolSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PoolSystem.Controllers
 {
